a1ca27352f4f454a3d988d909012e5e3738def3f
Mlynnik Semen <mlynnik@sfedu.ru>
2023-09-30 17:09:44 +0300

    Program output greeting on differents languages

--- hello.pas
+++ hello.pas
@@ -1,2 +1,11 @@
-﻿##
-Print('Привет мир!');
+﻿begin
+  var lang := ReadString('Select language: ru/en/de');
+  if lang = 'ru' then
+    Print('Добрый день')
+  else if lang = 'en' then
+    Print('Good day')
+  else if lang = 'de' then
+    Print('Guten Tag')
+  else
+    Print('Неизвестный язык');
+end.
